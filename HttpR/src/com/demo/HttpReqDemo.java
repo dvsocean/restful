b33bccf469fdb4d7473f322b703bb824fe20dc66
dvsocean <dvsocean@icloud.com>
2018-01-30 20:06:11 -0800

    added rest test

---
+++ HttpR/src/com/demo/HttpReqDemo.java
@@ -0,0 +1,54 @@
+package com.demo;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class HttpReqDemo {
+	//URL Encode the data
+	private static String params = "name=Jennifer&email=jenn2@yahoo.com&password=flyAway123";
+	private static String php_endpoint = "http://www.nerdschool.net/api_make_user";
+	
+	public static void main(String[] args) throws Exception {
+		String res = dummyHttpReqRes2(php_endpoint, params);
+		System.out.println(res);
+    } 
+	
+	public static String dummyHttpReqRes2(String targetURL, String urlParameters) throws IOException {
+		URL url = new URL(targetURL);
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        try {
+            //Create connection
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+
+            //set to true to allow setting method parameters
+            conn.setDoOutput(true);
+
+            //Request
+            DataOutputStream writer = new DataOutputStream (conn.getOutputStream());
+            writer.writeBytes(urlParameters);
+            writer.close();
+
+            //Response
+            BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            StringBuilder response = new StringBuilder(); 
+            String chunk;
+            while ((chunk = reader.readLine()) != null) {
+                response.append(chunk);
+            }
+            reader.close();
+            return response.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            if (conn != null) {
+                conn.disconnect();
+            }
+        }
+    }
+}
